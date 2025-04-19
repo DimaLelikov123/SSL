95252ad81d22d19ce2ef25b335d45a5597b59084
DimaLelikov123 <114059838+DimaLelikov123@users.noreply.github.com>
2025-04-20 02:27:03 +0300

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const https = require('https');
 const fs = require('fs');
+const forge = require('node-forge');
+
+const p12Buffer = fs.readFileSync('certs/server.p12');
+
+const p12Asn1 = forge.asn1.fromDer(p12Buffer.toString('binary'));
+const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, 'changeit');
+
+let keyObj, certObj;
+
+for (const safeContent of p12.safeContents) {
+    for (const safeBag of safeContent.safeBags) {
+        if (safeBag.type === forge.pki.oids.pkcs8ShroudedKeyBag) {
+            keyObj = forge.pki.privateKeyToPem(safeBag.key);
+        } else if (safeBag.type === forge.pki.oids.certBag) {
+            certObj = forge.pki.certificateToPem(safeBag.cert);
+        }
+    }
+}
 
 const options = {
-    key: fs.readFileSync('./certs/server.key'),
-    cert: fs.readFileSync('./certs/server.crt'),
+    key: keyObj,
+    cert: certObj,
 };
 
 https.createServer(options, (req, res) => {
